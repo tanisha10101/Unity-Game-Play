4c7d3fa2cb383df2310bdf3bde611442152c1ff6
TANISHA Bansal <115376761+tanisha10101@users.noreply.github.com>
2025-02-03 01:24:33 +0530

    Add files via upload

---
+++ Assets/Plugins/Gameplay system/AbilitySystemComponent.cs
@@ -0,0 +1,204 @@
+﻿using System.Linq;
+using System.Runtime.CompilerServices;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using GameplayAbilitySystem.ExtensionMethods;
+using GameplayAbilitySystem.Abilities;
+using GameplayAbilitySystem.Events;
+using GameplayAbilitySystem.GameplayEffects;
+using GameplayAbilitySystem.Interfaces;
+using UnityEngine;
+using GameplayAbilitySystem.Attributes;
+using UnityEngine.Events;
+using GameplayAbilitySystem.Enums;
+using GameplayAbilitySystem.GameplayCues;
+
+namespace GameplayAbilitySystem {
+    /// <inheritdoc />
+    [AddComponentMenu("Gameplay Ability System/Ability System")]
+    public class AbilitySystemComponent : MonoBehaviour, IGameplayAbilitySystem {
+        public Transform TargettingLocation;
+        
+        [SerializeField]
+        private GenericAbilityEvent _onGameplayAbilityActivated = new GenericAbilityEvent();
+        /// <inheritdoc />
+        public GenericAbilityEvent OnGameplayAbilityActivated => _onGameplayAbilityActivated;
+
+        [SerializeField]
+        private GenericAbilityEvent _onGameplayAbilityEnded = new GenericAbilityEvent();
+        /// <inheritdoc />
+        public GenericAbilityEvent OnGameplayAbilityEnded => _onGameplayAbilityEnded;
+
+        [SerializeField]
+        private GameplayEvent _onGameplayEvent = new GameplayEvent();
+        /// <inheritdoc />
+        public GameplayEvent OnGameplayEvent => _onGameplayEvent;
+
+        [SerializeField]
+        protected ActiveGameplayEffectsContainer _activeGameplayEffectsContainer;
+        /// <inheritdoc />
+        public ActiveGameplayEffectsContainer ActiveGameplayEffectsContainer => _activeGameplayEffectsContainer;
+
+        [SerializeField]
+        protected List<IGameplayAbility> _runningAbilities = new List<IGameplayAbility>();
+        /// <inheritdoc />
+        public List<IGameplayAbility> RunningAbilities => _runningAbilities;
+
+        [SerializeField]
+        protected GenericGameplayEffectEvent _onEffectAdded = new GenericGameplayEffectEvent();
+        /// <inheritdoc />
+        public GenericGameplayEffectEvent OnEffectAdded => _onEffectAdded;
+
+        [SerializeField]
+        protected GenericGameplayEffectEvent _onEffectRemoved = new GenericGameplayEffectEvent();
+        /// <inheritdoc />
+        public GenericGameplayEffectEvent OnEffectRemoved => _onEffectRemoved;
+
+        [SerializeField]
+        private GenericAbilityEvent _onGameplayAbilityCommitted = new GenericAbilityEvent();
+        /// <inheritdoc />
+        public GenericAbilityEvent OnGameplayAbilityCommitted => _onGameplayAbilityCommitted;
+
+        public void Awake() {
+            this._activeGameplayEffectsContainer = new ActiveGameplayEffectsContainer(this);
+        }
+        /// <inheritdoc />
+        public Transform GetActor() {
+            return this.transform;
+        }
+
+        void Update() {
+
+        }
+
+        /// <inheritdoc />
+        public void HandleGameplayEvent(GameplayTag EventTag, GameplayEventData Payload) {
+            /**
+             * TODO: Handle triggered abilities
+             * Search component for all abilities that are automatically triggered from a gameplay event
+             */
+
+            OnGameplayEvent.Invoke(EventTag, Payload);
+        }
+
+        /// <inheritdoc />
+        public void NotifyAbilityEnded(GameplayAbility ability) {
+            _runningAbilities.Remove(ability);
+        }
+
+        /// <inheritdoc />
+        public bool TryActivateAbility(GameplayAbility Ability) {
+            if (!this.CanActivateAbility(Ability)) return false;
+            if (!Ability.IsAbilityActivatable(this)) return false;
+            _runningAbilities.Add(Ability);
+            Ability.CommitAbility(this);
+
+            return true;
+        }
+
+        /// <inheritdoc />
+        public bool CanActivateAbility(GameplayAbility Ability) {
+            // Check if an ability is already active on this ASC
+            if (_runningAbilities.Count > 0) {
+                return false;
+            }
+
+            return true;
+        }
+
+        public async void ApplyBatchGameplayEffects(IEnumerable<(GameplayEffect Effect, IGameplayAbilitySystem Target, float Level)> BatchedGameplayEffects) {
+
+            var instantEffects = BatchedGameplayEffects.Where(x => x.Effect.GameplayEffectPolicy.DurationPolicy == Enums.EDurationPolicy.Instant);
+            var durationalEffects = BatchedGameplayEffects.Where(
+                x =>
+                    x.Effect.GameplayEffectPolicy.DurationPolicy == Enums.EDurationPolicy.HasDuration ||
+                    x.Effect.GameplayEffectPolicy.DurationPolicy == Enums.EDurationPolicy.Infinite
+                    );
+
+            // Apply instant effects
+            foreach (var item in instantEffects) {
+                if (await ApplyGameEffectToTarget(item.Effect, item.Target)) {
+                    // item.Target.AddGameplayEffectToActiveList(Effect);
+
+                }
+            }
+
+            // Apply durational effects
+            foreach (var effect in durationalEffects) {
+                if (await ApplyGameEffectToTarget(effect.Effect, effect.Target)) {
+
+                }
+            }
+
+        }
+
+        /// <inheritdoc />
+        public Task<GameplayEffect> ApplyGameEffectToTarget(GameplayEffect Effect, IGameplayAbilitySystem Target, float Level = 0) {
+            // TODO: Check to make sure all the attributes being modified by this gameplay effect exist on the target
+
+            // TODO: Get list of tags owned by target
+
+            // TODO: Check for immunity tags, and don't apply gameplay effect if target is immune (and also add Immunity Tags container to IGameplayEffect)
+
+            // TODO: Check to make sure Application Tag Requirements are met (i.e. target has the required tags, if any)
+
+            // If this is a non-instant gameplay effect (i.e. it will modify the current value, not the base value)
+
+            // If this is an instant gameplay effect (i.e. it will modify the base value)
+
+            // Handling Instant effects is different to handling HasDuration and Infinite effects
+            if (Effect.GameplayEffectPolicy.DurationPolicy == Enums.EDurationPolicy.Instant) {
+                Effect.ApplyInstantEffect(Target);
+            } else {
+                // Durational effects require attention to many more things than instant effects
+                // Such as stacking and effect durations
+                var EffectData = new ActiveGameplayEffectData(Effect, this, Target);
+                _ = Target.ActiveGameplayEffectsContainer.ApplyGameEffect(EffectData);
+            }
+
+            var gameplayCues = Effect.GameplayCues;
+            // Execute gameplay cue
+            for (var i = 0; i < gameplayCues.Count; i++) {
+                var cue = gameplayCues[i];
+                cue.HandleGameplayCue(Target.GetActor().gameObject,new GameplayCueParameters(null, null, null), EGameplayCueEvent.OnActive);
+            }
+
+            return Task.FromResult(Effect);
+        }
+
+
+        /// <inheritdoc />
+        public float GetNumericAttributeBase(AttributeType AttributeType) {
+            var attributeSet = this.GetComponent<AttributeSet>();
+            var attribute = attributeSet.Attributes.FirstOrDefault(x => x.AttributeType == AttributeType);
+            if (attribute == null) return 0;
+            return attribute.BaseValue;
+        }
+
+        /// <inheritdoc />
+        public float GetNumericAttributeCurrent(AttributeType AttributeType) {
+            var attributeSet = this.GetComponent<AttributeSet>();
+            return attributeSet.Attributes.FirstOrDefault(x => x.AttributeType == AttributeType).CurrentValue;
+        }
+
+        public void SetNumericAttributeBase(AttributeType AttributeType, float modifier) {
+            var attributeSet = this.GetComponent<AttributeSet>();
+            var attribute = attributeSet.Attributes.FirstOrDefault(x => x.AttributeType == AttributeType);
+            var newValue = modifier;
+            attribute.SetAttributeBaseValue(attributeSet, ref newValue);
+        }
+
+        public void SetNumericAttributeCurrent(AttributeType AttributeType, float NewValue) {
+            var attributeSet = this.GetComponent<AttributeSet>();
+            var attribute = attributeSet.Attributes.FirstOrDefault(x => x.AttributeType == AttributeType);
+            attribute.SetAttributeCurrentValue(attributeSet, ref NewValue);
+        }
+
+    }
+
+
+
+
+}
---
+++ Assets/Plugins/Gameplay system/GameplayAbility.cs
@@ -0,0 +1,183 @@
+using System.Collections.Generic;
+using GameplayAbilitySystem.Events;
+using GameplayAbilitySystem.Interfaces;
+using GameplayAbilitySystem.GameplayEffects;
+using UnityEngine;
+using GameplayAbilitySystem.Abilities.AbilityActivations;
+using System.Linq;
+
+namespace GameplayAbilitySystem.Abilities {
+    /// <inheritdoc />
+    [AddComponentMenu("Ability System/Ability")]
+    [CreateAssetMenu(fileName = "Ability", menuName = "Ability System/Ability")]
+    public class GameplayAbility : ScriptableObject, IGameplayAbility {
+
+        [SerializeField]
+        private GameplayAbilityTags _tags = new GameplayAbilityTags();
+
+        [SerializeField]
+        private GameplayCost _gameplayCost = new GameplayCost();
+
+        [SerializeField]
+        private List<GameplayCooldown> _gameplayCooldowns = new List<GameplayCooldown>();
+
+        [SerializeField]
+        private GenericAbilityEvent _onGameplayAbilityCommitted = new GenericAbilityEvent();
+
+
+        [SerializeField]
+        private GenericAbilityEvent _onGameplayAbilityCancelled = new GenericAbilityEvent();
+
+
+        [SerializeField]
+        private GenericAbilityEvent _onGameplayAbilityEnded = new GenericAbilityEvent();
+
+        [SerializeField]
+        private AbstractAbilityActivation _abilityLogic = null;
+
+
+
+        /// <inheritdoc />
+        public IAbilityTags Tags => _tags;
+        /// <inheritdoc />
+        public IGameplayCost GameplayCost => _gameplayCost;
+        /// <inheritdoc />
+        public List<GameplayCooldown> GameplayCooldowns => _gameplayCooldowns;
+        /// <inheritdoc />
+        public GenericAbilityEvent OnGameplayAbilityCommitted => _onGameplayAbilityCommitted;
+        /// <inheritdoc />
+        public GenericAbilityEvent OnGameplayAbilityCancelled => _onGameplayAbilityCancelled;
+        /// <inheritdoc />
+        public GenericAbilityEvent OnGameplayAbilityEnded => _onGameplayAbilityEnded;
+        /// <inheritdoc />
+        public AbstractAbilityActivation AbilityLogic => _abilityLogic;
+
+
+
+        void Start() {
+        }
+
+
+
+        protected void ApplyGameplayEffectToTarget(GameplayEffect effect, AbilitySystemComponent target) {
+            //
+        }
+
+        /// <inheritdoc />
+        public virtual void ActivateAbility(IGameplayAbilitySystem AbilitySystem) {
+            _abilityLogic.ActivateAbility(AbilitySystem, this);
+            ApplyCooldown(AbilitySystem);
+        }
+
+        /// <inheritdoc />
+        public virtual bool IsAbilityActivatable(IGameplayAbilitySystem AbilitySystem) {
+            return PlayerHasResourceToCast(AbilitySystem) && AbilityOffCooldown(AbilitySystem);
+        }
+
+        /// <summary>
+        /// Applies the ability cost, decreasing the specified cost resource from the player.
+        /// If player doesn't have the required resource, the resource goes to negative (or clamps to 0)
+        /// </summary>
+        protected void ApplyCost(IGameplayAbilitySystem AbilitySystem) {
+            var modifiers = this.GameplayCost.CostGameplayEffect.CalculateModifierEffect();
+            var attributeModification = this.GameplayCost.CostGameplayEffect.CalculateAttributeModification(AbilitySystem, modifiers);
+            this.GameplayCost.CostGameplayEffect.ApplyInstantEffect(AbilitySystem);
+
+        }
+
+        /// <summary>
+        /// Applies cooldown.  Cooldown is applied even if the  ability is already
+        /// on cooldown
+        /// </summary>
+        protected void ApplyCooldown(IGameplayAbilitySystem abilitySystem) {
+            foreach (var cooldown in this.GameplayCooldowns) {
+                abilitySystem.ActiveGameplayEffectsContainer.ApplyCooldownEffect(new ActiveGameplayEffectData(cooldown.CooldownGameplayEffect, abilitySystem, abilitySystem));
+            }
+        }
+
+        /// <inheritdoc />
+        public void EndAbility(IGameplayAbilitySystem AbilitySystem) {
+            _onGameplayAbilityEnded.Invoke(this);
+
+            // Ability finished.  Remove all listeners.
+            _onGameplayAbilityEnded.RemoveAllListeners();
+
+            // TODO: Remove tags added by this ability
+
+            // TODO: Cancel all tasks?
+
+            // TODO: Remove gameplay cues
+
+            // TODO: Cancel ability
+
+            // TODO: Remove blocking/cancelling Gameplay Tags
+
+            // Tell ability system ability has ended
+            AbilitySystem.NotifyAbilityEnded(this);
+        }
+
+
+        /// <inheritdoc />
+        public bool PlayerHasResourceToCast(IGameplayAbilitySystem AbilitySystem) {
+            // Check the modifiers on the ability cost GameEffect
+            var modifiers = this.GameplayCost.CostGameplayEffect.CalculateModifierEffect();
+            var attributeModification = this.GameplayCost.CostGameplayEffect.CalculateAttributeModification(AbilitySystem, modifiers, operateOnCurrentValue: true);
+
+            foreach (var attribute in attributeModification) {
+                if (attribute.Value.NewAttribueValue < 0) return false;
+            }
+            return true;
+        }
+
+        /// <inheritdoc />
+        public bool CommitAbility(IGameplayAbilitySystem AbilitySystem) {
+            ActivateAbility(AbilitySystem);
+            AbilitySystem.OnGameplayAbilityActivated.Invoke(this);
+            ApplyCost(AbilitySystem);
+            return true;
+        }
+
+        /// <inheritdoc />
+        public bool AbilityOffCooldown(IGameplayAbilitySystem AbilitySystem) {
+            var cooldownTags = this.GetAbilityCooldownTags();
+
+            // Check if we have the cooldown effect
+            var cooldownEffectsMatched = AbilitySystem.ActiveGameplayEffectsContainer.ActiveCooldowns.Select(x => x.Effect)
+                .Intersect(this.GameplayCooldowns.Select(x => x.CooldownGameplayEffect)).Count();
+
+
+
+            return cooldownEffectsMatched == 0;
+        }
+
+        /// <inheritdoc />
+        public List<GameplayTag> GetAbilityCooldownTags() {
+            var tags = new List<GameplayTag>();
+            for (var i = 0; i < this._gameplayCooldowns.Count; i++) {
+                tags.AddRange(this._gameplayCooldowns[i].GetCooldownTags());
+            }
+
+            return tags;
+        }
+
+        public (float CooldownElapsed, float CooldownTotal) CalculateCooldown(IGameplayAbilitySystem AbilitySystem) {
+            var dominantCooldown = this.GameplayCooldowns
+                                .Select(abilityCooldown => abilityCooldown.CooldownGameplayEffect)
+                                .Select(abilityCooldown =>
+                                            AbilitySystem.ActiveGameplayEffectsContainer.ActiveCooldowns
+                                                .FirstOrDefault(activeCooldownOnCharacter => activeCooldownOnCharacter.Effect == abilityCooldown))
+
+                                .Where(x => x != null && x.Effect != null)
+                                .DefaultIfEmpty()
+                                .OrderByDescending(activeEffect => activeEffect?.CooldownTimeRemaining)
+                                .FirstOrDefault();
+
+            if (dominantCooldown == null) {
+                return (0f, 0f);
+            }
+
+            return (dominantCooldown.CooldownTimeElapsed, dominantCooldown.CooldownTimeTotal);
+        }
+
+    }
+}
